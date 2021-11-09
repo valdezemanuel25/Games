462e9f224da1ea87731ed3fdbefd6d45314a5fd6
valdezemanuel25 <42313336+valdezemanuel25@users.noreply.github.com>
2021-11-09 12:47:44 -0600

    Add files via upload

---
+++ computerguess.py
@@ -0,0 +1,27 @@
+import random
+
+def computer_guess(x):
+    # this is the range of our guessing numbers
+    low = 1
+    high =x
+    feedback = ''
+
+    # while the computer guess is not correct then it will jump into this loop
+    while feedback != 'c':
+        if low!=high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'IS {guess} too high [H], too low [L], or correct [C]\n').lower()
+        while feedback != 'h' and feedback !='l' and feedback!='c':
+            feedback = input("Please enter H for too High, L for too low or C for correct\n")
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+
+    print(f"Computer has guessed {guess} ")
+
+# this calls the function of computer guess and passes 10 as the x parameter
+computer_guess(10 )
---
+++ guessnumber.py
@@ -0,0 +1,19 @@
+import random
+
+def guess(x):
+    # this sets the random # to a random integer between 1 and x
+    random_number = random.randint(1,x)
+    # guess is 0 becuase we need something outside the range to jump into the while loop
+    guess = 0
+
+    while guess!=random_number:
+        # convert the string input to an integer to compare a number with a number
+        guess = int(input(f'Guess a number betwwen 1 and {x}\n'))
+        if guess > random_number:
+            print("Guess lower")
+        elif guess < random_number:
+            print("Guess higher")
+    print(f'Correct you guessed {random_number}')
+
+# this calls the function of guess and passes 10 as the x parameter
+guess(10)
